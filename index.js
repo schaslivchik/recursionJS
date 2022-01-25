3eae6c2b29f53a261a98a51a6312bbab30deb75c
Mikhail <schaslivchikm@gmail.com>
2022-01-25 21:27:37 +0200

    initial

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//1
+function pow(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  }
+  return base * pow(base, exponent - 1);
+}
+console.log("pow(2,3) :>> ", pow(2, 3));
+
+//2
+function bracketWrapper(n) {
+
+  bracketWrapper(n);
+
+  function bracketWrapper(n) {
+    if (n === 0) {
+      return;
+    }
+    console.log("(");
+    bracketWrapper(n - 1);
+    console.log(")");
+  }
+}
+
+bracketWrapper(3);
